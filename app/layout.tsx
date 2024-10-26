c2bcd8cdbada9f6908d23a15d780d6de3b01e6c7
Tiago Moutinho <tiagoagm@gmail.com>
2024-10-26 11:08:23 +0100

    Add images

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,10 +26,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Script src="https://s3.tradingview.com/tv.js" strategy="beforeInteractive" />
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,81 @@
-import Image from "next/image";
+
+"use client";
+
+import { useEffect } from "react";
+import { FaTelegram, FaTwitter } from "react-icons/fa";
+
+declare global {
+  interface Window {
+    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+    TradingView: any;
+  }
+}
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+  useEffect(() => {
+    if (typeof window !== 'undefined' && window.TradingView) {
+      new window.TradingView.widget({
+        symbol: "0x006d197201d162343D62C9F9759F0758336dF560",
+        interval: "1",
+        locale: "en",
+        clientId: "0",
+        userId: "0",
+        chartsStorageVer: "1.0",
+        debug: false,
+        timezone: "Etc/UTC",
+        theme: "dark",
+        container_id: "tradingview_chart"
+      });
+    }
+  }, []);
+
+  return (
+    <div className="bg-gradient-to-br from-yellow-300 to-blue-400 min-h-screen font-sans text-gray-900 py-20">
+      <main className="container mx-auto p-6">
+        <h1 className="text-4xl font-extrabold text-center text-red-600 mt-4" style={{ fontFamily: 'Comic Sans MS, sans-serif' }}>
+          Plusle to Pulse ピカとピコ  ⚡
+        </h1>
+        <p className="text-center text-gray-700 mt-8 text-2xl">
+          Plusle is an Electric-type Pokémon known for its upbeat and supportive nature, often seen cheering on its teammates with boundless energy. With red, rabbit-like ears and a small, white body, Plusle has positive-themed markings, including a “+” symbol on its cheeks and a red plus-shaped tail. It generates sparks of electricity to motivate others, especially its partner Minun. Plusle is a friendly, energetic Pokémon, popular for its cheerleader-like spirit and ability to boost the morale of those around it.
+        </p>
+
+        <div className="flex items-center justify-center mt-8 gap-6">
+          <a href="https://t.me/PlusleToPulse" target="_blank" rel="noreferrer">
+            <FaTelegram size="2em" />
           </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
+          <a href="https://x.com/plusletopulse" target="_blank" rel="noreferrer">
+            <FaTwitter size="2em" />
           </a>
         </div>
+
+        <div className="text-center flex items-center justify-center mt-8 py-14">
+          <a
+            href="https://coin.quest/Pulsechain/0x006d197201d162343D62C9F9759F0758336dF560"
+            target="_blank"
+            className="bg-yellow-400 flex gap-2 w-52 justify-center items-center text-blue-600 font-bold border text-4xl py-2 px-4 rounded hover:bg-yellow-500 transition"
+            rel="noreferrer"
+          >
+            <img src="/pulsechain.png" alt="Pulsechain Logo" className="h-16" />
+            Buy
+          </a>
+        </div>
+
+        <div className="flex items-center justify-center mt-8 gap-6 py-10">
+          <img src="/1.webp" alt="Plusle 1" className="h-44" />
+          <img src="/2.png" alt="Plusle 2" className="h-44" />
+          <img src="/3.jpg" alt="Plusle 2" className="h-44" />
+          <img src="/4.png" alt="Plusle 2" className="h-44" />
+        </div>
+
+        <div className="mt-12">
+          <h2 className="text-3xl font-bold text-center text-red-600 mb-4">Plusle Token Price Chart</h2>
+          <div className="">
+            <div id="tradingview_chart" className="w-full h-[500px]" />
+          </div>
+        </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
